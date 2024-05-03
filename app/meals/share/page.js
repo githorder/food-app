70f71c5c798b69c802982c64d617097869b38359
Diyor <mygit.2018@gmail.com>
2024-05-03 17:46:33 +0500

    add form submit status btn

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,11 @@
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
 import classes from "./page.module.css";
 
+import { shareMeal } from "@/lib/actions";
+
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    "use server";
-
-    const meal = {
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image"),
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-
-    console.log(meal);
-  }
-
   return (
     <>
       <header className={classes.header}>
@@ -56,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
